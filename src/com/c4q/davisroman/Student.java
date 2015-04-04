58c420cb7a2fbd23c8818a556ffee8f25949dc49
Davis Roman <davis.roman84@gmail.com>
2015-04-04 07:59:09 -0400

    initial commit

---
+++ src/com/c4q/davisroman/Student.java
@@ -0,0 +1,26 @@
+package com.c4q.davisroman;
+
+/**
+ * Created by davis on 4/4/15.
+ */
+public class Student {
+    private String name;
+    private int numOfPencilsInPocket;
+
+    public Student(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getNumOfPencilsInPocket(){
+        return numOfPencilsInPocket;
+    }
+    public void setNumOfPencilsInPocket(int numOfPencilsInPocket){
+        this.numOfPencilsInPocket = numOfPencilsInPocket;
+    }
+    public void setLunchBox(lunchbox item){
+        //FIXME
+    }
+}
---
+++ src/com/c4q/davisroman/Teacher.java
@@ -0,0 +1,17 @@
+package com.c4q.davisroman;
+
+/**
+ * Created by davis on 4/4/15.
+ */
+public class Teacher {
+    private String name;
+    public Teacher(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void addStudent(Student student){
+        //FIXME
+    }
+}
